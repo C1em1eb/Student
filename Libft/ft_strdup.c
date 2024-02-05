658055df6f8b0b813c458ea2e1849467eba432b7
Clement Leblond <bluesky_@live.fr>
2024-02-05 15:46:04 +0100

    ft_strdrup.c

---
+++ Libft/ft_strdup.c
@@ -0,0 +1,28 @@
+#include <string.h>
+char	*ft_strdup(char *src)
+{
+	int		i;
+	int		j;
+	char	*dest;
+
+	j = 0;
+	if(src == NULL)
+	return (0);
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	dest = malloc((j + 1) * sizeof(char));
+	if (dest == NULL)
+	{
+		exit (0);
+	}
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
