03179f1e2a122d86bf70683f270e5461db48c87f
Clement Leblond <cleblond@c2r8s6.42prague.com>
2024-02-19 16:08:17 +0100

    42 header

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -1,40 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cleblond <cleblond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/19 16:05:54 by cleblond          #+#    #+#             */
-/*   Updated: 2024/02/19 16:05:55 by cleblond         ###   ########.fr       */
+/*   Created: 2024/02/19 16:03:44 by cleblond          #+#    #+#             */
+/*   Updated: 2024/02/19 16:07:02 by cleblond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const	*s, unsigned int	start, size_t	len)
-{
-	unsigned int i;
-	char	*substr;
-
-	i = 0;
-	substr = NULL;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	substr = malloc(sizeof(char) * (len + 1));
-	if (substr == NULL)
-		return (0);
-	if (start > i || (start + len) > i)
-		return (NULL);
-	i = 0;
-	while ( i < len)
-	{
-		substr[i] = s[start];
-		i++;
-		start++;
-	}
-	substr[i] = '\0';
-	return (substr);
-}
