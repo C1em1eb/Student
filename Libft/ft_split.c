3c3670ec8c6738ab953fda57d2ee4b578145ed69
Clement Leblond <bluesky_@live.fr>
2024-02-19 12:28:47 +0100

    ft_split

---
+++ Libft/ft_split.c
@@ -0,0 +1,84 @@
+#include "libft.h"
+
+char	**ft_split(char const *s, char c);
+static int ft_wordcounter(char const *s, char c);
+
+/*
+["S", "a", "l", "t", ",", " "],
+["C", "l", "e", "m", "e", "n"]
+ */
+
+int main(void)
+{
+	char 	s[] = "Hello World";
+	char 	**result = NULL;
+	int i = 0;
+	result = ft_split(s, ' ');
+
+	while (i < 6)
+	{
+		int j = 0;
+		while (j < 3)
+		{
+			printf("%c", result[i][j]);
+			j++;
+		}
+	printf("\n");
+	i++;
+	}
+	return (0);
+}
+
+static int ft_wordcounter(char const *s, char c)
+{
+	int i;
+	int wordcounter;
+
+	i = 0;
+	wordcounter = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			i++;
+		if (s[i] != c && s[i] != '\0')
+			wordcounter++;
+		while (s[i] != c && s[i] != '\0')
+			i++;
+	}
+	return (wordcounter);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		wordcount;
+	char	**split;
+
+	if (s == NULL)
+		return (NULL);
+
+	wordcount = ft_wordcounter(s, c);
+	split = (char **)malloc(sizeof(char *) * (wordcount + 1));
+	if (split == NULL)
+		return (NULL);
+
+	int j = 0;
+	while (*s)
+	{
+		while (*s == c)
+		s++;
+
+		if (*s && *s != c)
+		{
+			const char *end = ft_strrchr(s, c);
+			size_t len = (end != NULL) ? (size_t)(end - s + 1) : strlen(s);
+
+			split[j] = ft_substr(s, 0, len);
+			if (split[j] == NULL)
+				return (NULL);
+	s += len;
+	j++;
+	}
+	}
+	split[j] = NULL;
+	return (split);
+}
--- Libft/libft.h
+++ Libft/libft.h
@@ -17,6 +17,7 @@ void	*ft_memchr(const void	*s, int c, size_t	n);
 int		ft_memcmp(const void	*s1, const void	*s2, size_t	n);
 void	*ft_memcpy(void	*dest, const void	*src, size_t	n);
 void	*ft_memmove(void	*dest, const void	*src, size_t	n);
+char 	**ft_split(char const *s, char c);
 void	*ft_memset(void	*s, int c, size_t	n);
 char	*ft_strchr(const char	*s, int c);
 char	*ft_strdup(char	*src);
