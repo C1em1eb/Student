09842697c97f8528a18fc831f023f720330eaca8
Clement Leblond <cleblond@c2r8s4.42prague.com>
2024-02-21 18:07:46 +0100

    bonus part1

--- Libft/ft_lstadd_front.c
+++ Libft/ft_lstadd_front.c
@@ -1,16 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cleblond <cleblond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/21 15:01:49 by cleblond          #+#    #+#             */
-/*   Updated: 2024/02/21 15:01:56 by cleblond         ###   ########.fr       */
+/*   Created: 2024/02/21 18:04:04 by cleblond          #+#    #+#             */
+/*   Updated: 2024/02/21 18:07:28 by cleblond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-t_list *ft_lstnew(void *content)
+void ft_lstadd_front(t_list **lst, t_list *new)
 {
+	t_list	*node;
 
+	node = malloc(sizeof(t_list));
+	if (node == NULL)
+		return (NULL);
+	node->content = new;
+	node->next = lst;
+	return	(node);
 }
+
+new->next = *lst;
+*lst = new;
---
+++ Libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cleblond <cleblond@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 15:01:49 by cleblond          #+#    #+#             */
+/*   Updated: 2024/02/21 17:56:06 by cleblond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list *ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = malloc(sizeof(t_list));
+	if (node == NULL)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return	(node);
+}
