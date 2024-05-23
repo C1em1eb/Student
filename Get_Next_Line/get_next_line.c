b3c44f3f8bffdd5e44c7de404f63482cdb020705
Clement Leblond <bluesky_@live.fr>
2024-05-23 14:56:47 +0200

    updade

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -1,6 +1,6 @@
 #include "get_next_line.h"
 
-/* int main(void)
+int main(void)
 {
 	int		fd;
 	fd = open("text", O_RDONLY);
@@ -12,7 +12,7 @@
 	printf("%s", get_next_line(fd));
 	close (fd);
 	return (0);
-} */
+}
 
 char	*get_next_line(int fd)
 {
@@ -20,21 +20,26 @@ char	*get_next_line(int fd)
 	char 	*temp;
 	static char 	*stash;
 	char 	*line;
-	char 	*pdollar;
-	int		idollar;
+	char 	*p_separator;
+	int		len_to_separator;
 	size_t 	bytes_read;
 
 	buffer = NULL;
+	stash = NULL;
 	buffer = malloc(sizeof(char)* BUFFER_SIZE + 1);
 	if (buffer == NULL)
 		return (NULL);
-	stash = malloc(sizeof(char)* (BUFFER_SIZE + 1) * 100);
+	stash = malloc(sizeof(char)* (BUFFER_SIZE + 1));
 	if (stash == NULL)
 		return (NULL);
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	temp = malloc(sizeof(char)* (BUFFER_SIZE + 1));
+	if (temp == NULL)
+		return (NULL);
+	if (fd < 0 || BUFFER_SIZE <= 0)
 	{
 		free (buffer);
 		free (stash);
+		free (temp);
 		return (NULL);
 	}
 	stash[0] = '\0';
@@ -45,23 +50,22 @@ char	*get_next_line(int fd)
 		temp = ft_strdup(buffer);
 		stash = ft_strjoin(stash, temp);
 		free (temp);
-		pdollar = ft_strchr(stash, '\n');
-		if (pdollar != NULL)
+		p_separator = ft_strchr(stash, '\n');
+		if (p_separator != NULL)
 		{
-			idollar = pdollar - stash;
-			line = ft_substr(stash, 0, idollar + 1);
-			stash = ft_substr(stash, idollar + 1, BUFFER_SIZE + 50);
+			len_to_separator = p_separator - stash + 1;
+			line = ft_substr(stash, 0, len_to_separator + 1);
+			temp = ft_strdup(stash);
+			free (stash);
+			stash = temp;
 			return (line);
 			free(line);
 		}
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 	}
 	if (bytes_read < 1)
-	{
-		free (buffer);
 		free (stash);
-		return (NULL);
-	}
+
 	free (buffer);
 	return (NULL);
 }
