edccf14c6a7ae642552f1df5b70bbce437b04384
Clement Leblond <bluesky_@live.fr>
2024-06-30 15:55:49 +0200

    updated

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: cleblond <cleblond@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/28 15:30:19 by cleblond          #+#    #+#             */
-/*   Updated: 2024/05/28 16:28:16 by cleblond         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
 /* int main(void)
@@ -17,18 +5,15 @@
 	int		fd;
 	char	*line;
 
-	//fd = open("gnlTester/files/alternate_line_nl_no_nl", O_RDONLY);
 	fd = open("text", O_RDONLY);
-
 	while (1)
 	{
 		line = get_next_line(fd);
 		printf("%s", line);
- 		if (line == NULL)
+		if (line == NULL)
 			break;
 		free (line);
 	}
-	free (line);
 	close (fd);
 	return (0);
 } */
@@ -43,7 +28,7 @@ char	*get_next_line(int fd)
 	int			len_to_separator;
 	ssize_t		bytes_read;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 
 	buffer = malloc(sizeof(char)* (BUFFER_SIZE + 1));
@@ -51,34 +36,37 @@ char	*get_next_line(int fd)
 		return (NULL);
 	line = NULL;
 	if (stash == NULL)
-		stash = ft_strdup("");
-	//printf("stash initial : %s\n", stash);
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read == 0 || bytes_read == -1)
 	{
-		free (stash);
-		free (buffer);
-		return (NULL);
+		stash = ft_strdup("");
+		if (stash == NULL)
+		{
+			free(buffer);
+			return (NULL);
+		}
 	}
+	stash[ft_strlen(stash)] = '\0';
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
 	while (bytes_read > 0)
 	{
 		buffer[bytes_read] = '\0';
 		temp = ft_strjoin(stash, buffer);
-		//printf("1st temp %s\n", temp);
 		free (stash);
-		stash = NULL;
 		stash = temp;
 		p_separator = ft_strchr(stash, '\n');
 		if (p_separator != NULL)
 		{
 			len_to_separator =  p_separator - stash + 1;
 			line = ft_substr(stash, 0, len_to_separator);
+			if (line == NULL)
+			{
+				free(stash);
+				free(buffer);
+				return (NULL);
+			}
 			temp = ft_strdup(stash + len_to_separator);
 			free (stash);
-			stash = NULL;
 			stash = temp;
 			free (buffer);
-			buffer = NULL;
 			return (line);
 		}
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
@@ -86,21 +74,14 @@ char	*get_next_line(int fd)
 	if (bytes_read == 0 && stash[0] != '\0')
 	{
 		line = ft_strdup(stash);
-		free (stash);
+		free(stash);
 		stash = NULL;
 		free (buffer);
-		buffer = NULL;
-		//printf("final line %s\n", line);
 		return (line);
 	}
-	if (bytes_read == -1)
-	{
-		free (stash);
-		stash = NULL;
-		free (buffer);
-		buffer = NULL;
-		return (NULL);
-	}
+	free (stash);
+	stash = NULL;
+	free (buffer);
 	return (NULL);
 }
 
@@ -109,4 +90,3 @@ void free_and_null(char **ptr)
 	free (*ptr);
 	*ptr = NULL;
 }
-
