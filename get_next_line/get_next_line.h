79d14ef1afa8469c3399a3348fa7fc33d9ae7382
Clement Leblond <cleblond@c3r4s1.42prague.com>
2024-07-12 14:30:11 +0200

    gnl final

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: cleblond <cleblond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 14:27:24 by cleblond          #+#    #+#             */
-/*   Updated: 2024/07/12 14:27:33 by cleblond         ###   ########.fr       */
+/*   Updated: 2024/07/12 14:30:06 by cleblond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,10 @@ size_t	ft_strlen(const char *s);
 char	*ft_strjoin(const char *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-void	ft_free_and_null(char **ptr);
+ssize_t	read_and_process(int fd, char *buffer, char **stash, char **line);
+bool	init_stash(char **stash);
+char	*finalize_line(char **stash, char *buffer, char **line);
 char	*get_next_line(int fd);
+void	ft_free_and_null(char **ptr);
 
 #endif
